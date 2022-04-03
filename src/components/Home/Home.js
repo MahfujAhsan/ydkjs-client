70f5a5e33f3ff0260241ee60bfdfd69dc071349c
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-04-04 02:49:55 +0600

    review-users-updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import bannerImg from '../../images/product.png'
+import Reviews from '../Reviews/Reviews';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='w-3/4 mx-auto'>
+            <div className="text-center md:text-left my-16 flex flex-col-reverse md:grid grid-cols-5 md:gap-24 items-center">
+                <div className='col-span-3'>
+                    <div className='text-2xl md:text-5xl font-mono font-bold'>
+                        <h1 className='text-yellow-600'>Developer Guides</h1>
+                        <h2 className='text-black mt-2 md:mt-3'>Grow Your JS Knowledge</h2>
+                    </div>
+                    <p className='mt-4 font-serif leading-7 md:text-xl md:leading-9'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci ratione veritatis ut repellendus quidem eius, sapiente aspernatur quisquam! Est fugiat aliquid dolorum quis corrupti error facilis, cupiditate voluptas obcaecati molestias.</p>
+                    <button className='mt-5 border-2 rounded text-sm md:text-2xl font-bold font-mono px-4 py-2 border-yellow-600 hover:bg-black hover:text-white hover:border-black'>Live Demo</button>
+                </div>
+                <div className='col-span-2 '>
+                    <img className='w-2/4 mx-auto md:w-full' src={bannerImg} alt="productImg" />
+                </div>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-24 my-12'>
+                {
+                    users.slice(0, 3).map(user => <Reviews key={user._id} user={user}></Reviews>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,15 @@
+import { StarIcon } from '@heroicons/react/solid';
 import React from 'react';
 
-const Reviews = () => {
+const Reviews = (props) => {
+    const {name, img, reviewTxt, ratings, id} = props.user;
+    console.log(name)
     return (
-        <div>
-            <h2>Reviews</h2>
+        <div className='text-center border border-black p-4 rounded-lg'>
+            <img className='w-24 h-24 mx-auto rounded-full' src={img} alt="" />
+            <h2 className='text-xl font-bold font-mono my-2'>{name}</h2>
+            <p className='leading-8 font-serif'><span className='text-2xl text-yellow-600'>❝</span> {reviewTxt} <span className='text-2xl text-yellow-600'>❞</span></p>
+            <p className='mt-5 font-mono text-xl font-bold'>Ratings: <span className='text-4xl text-yellow-600'>{ratings}</span> stars</p>
         </div>
     );
 };
