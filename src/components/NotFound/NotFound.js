9bd2cf5278133e8730283803d5d4d77cad86fbc5
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-04-04 07:54:28 +0600

    404 updated

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import errorImg from '../../images/404.png'
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Not Found</h1>
+        <div className='w-3/4 mx-auto h-[80vh] mt-6'>
+            <h1 className='text-4xl md:text-6xl font-mono font-bold text-center text-red-600 uppercase'>Error: 404</h1>
+            <h1 className='text-3xl md:text-4xl font-mono font-bold text-center mt-3 md:mt-6'>Page Not Found</h1>
+            <img className='w-[200] mx-auto mt-[-30px]' src={errorImg} alt="" />
         </div>
     );
 };
